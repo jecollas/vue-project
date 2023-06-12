ff8902168050324a16453c013249d991e1bffdd1
Jacquelyn Collas <jackie@moreira.com>
2023-06-12 00:54:22 -0400

    test

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../ColorSelect.vue'
+import ColorSelect from '../ColorSelect.vue'
 
 describe('ColorSelect', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(ColorSelect, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
